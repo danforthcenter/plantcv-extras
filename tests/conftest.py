5351bada4533c6774721b0862ad538d892f02dd0
AFis-245 <annika.fischer@e-box3.de>
2024-12-03 16:58:11 -0600

    added blank line to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,13 @@ import matplotlib
 # Disable plotting
 matplotlib.use("Template")
 
+
 class TestData:
     def __init__(self):
         """Initialize simple variables."""
         # Test data directory
         self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
+
 
 @pytest.fixture(scope="session")
 def test_data():

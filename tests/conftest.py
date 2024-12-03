ce511f295beac4051a437adf110946c107057413
annikafischer <145135602+AFis-245@users.noreply.github.com>
2024-12-03 14:58:03 -0600

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+import matplotlib
+
+# Disable plotting
+matplotlib.use("Template")
+
+class TestData:
+    def __init__(self):
+        """Initialize simple variables."""
+        # Test data directory
+        self.datadir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata")
+
+@pytest.fixture(scope="session")
+def test_data():
+    """Test data object for the PlantCV-extras package."""
+    return TestData()

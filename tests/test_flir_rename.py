38ad6f9c6e4058054158811c97cd8b49f928cd67
AFis-245 <annika.fischer@e-box3.de>
2024-12-16 11:01:07 -0600

    changed filename to datadir.testdata

--- tests/test_flir_rename.py
+++ tests/test_flir_rename.py
@@ -1,31 +1,28 @@
 import os
 import pytest
 from plantcv.extras.thermal import flir_rename
-from plantcv.plantcv import readimage
 
 
 # tests fatal error if file doesn't exist or cannot be found
 def test_flir_rename_fatal_filename(test_data):
     """Test for PlantCV.Extras"""
+    cache_dir = tmpdir.mkdir("cache")
     with pytest.raises(RuntimeError):
-        flir_rename(filename="test.jpg", sep='_', new_dir=None)
+        flir_rename(filename="test.jpg", new_dir=cache_dir, sep='_')
 
 
 # tests fatal error if separator is not dash or underscore
 def test_flir_rename_fatal_sep(test_data):
     """Test for PlantCV.Extras"""
-    # Read in a test pseudo colored thermal image
-    _, filename, _ = readimage(test_data.thermal_img)
+    cache_dir = tmpdir.mkdir("cache")
     with pytest.raises(RuntimeError):
-        flir_rename(filename, sep='T', new_dir=None)
+        flir_rename(datadir.testdata, new_dir=cache_dir, sep='T')
 
 
 # tests dash as separator and copies the renamed image into a temp directory
 def test_flir_rename_newdir(test_data, tmpdir):
     """Test for PlantCV.Extras"""
     cache_dir = tmpdir.mkdir("cache")
-    # Read in a test pseudo colored thermal image
-    _, filename, _ = readimage(test_data.thermal_img)
-    flir_rename(filename, sep='-', new_dir=cache_dir)
+    flir_rename(datadir.testdata, new_dir=cache_dir, sep='-')
     newname = os.listdir(cache_dir)[0]
     assert newname == "FLIR5612-2024_05_16T15_47.jpg"

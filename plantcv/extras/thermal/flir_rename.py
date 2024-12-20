6b051c9ed569f401d0f5792c62892c232129cd78
AFis-245 <annika.fischer@e-box3.de>
2024-12-20 10:50:56 -0600

    fixed some deepsource errors

--- plantcv/extras/thermal/flir_rename.py
+++ plantcv/extras/thermal/flir_rename.py
@@ -42,6 +42,6 @@ def flir_rename(filename, new_dir, sep='_'):
     if sep == "-":
         date_format = "%Y_%m_%dT%H_%M"
         changename = "-filename<%f-$datetimeoriginal.%e"
-    result = subprocess.run(['which', 'exiftool'], capture_output=True, text=True)
+    result = subprocess.run(['which', 'exiftool'], capture_output=True, text=True, check=True)
     exiftool_path = result.stdout[:-1]
-    subprocess.run([exiftool_path, *args, date_format, changename, filename])
+    subprocess.run([exiftool_path, *args, date_format, changename, filename], check=True)
--- tests/test_flir_rename.py
+++ tests/test_flir_rename.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 from plantcv.extras.thermal import flir_rename
-import subprocess
+
 
 # tests fatal error if file doesn't exist or cannot be found
 def test_flir_rename_fatal_filename(test_data, tmpdir):
@@ -22,7 +22,6 @@ def test_flir_rename_fatal_sep(test_data, tmpdir):
 # tests dash as separator and copies the renamed image into a temp directory
 def test_flir_rename_newdir(test_data, tmpdir):
     """Test for PlantCV.Extras"""
-    #subprocess.run(["which", "exiftool"])
     cache_dir = tmpdir.mkdir("cache")
     flir_rename(test_data.thermal_img, new_dir=cache_dir, sep='-')
     newname = os.listdir(cache_dir)[0]

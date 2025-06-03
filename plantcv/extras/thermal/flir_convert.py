9770ceda5c8f7600177c3fc66c65b01dc21155b4
AFis-245 <annika.fischer@e-box3.de>
2025-06-03 11:29:02 -0500

    removed blank lines

--- plantcv/extras/thermal/flir_convert.py
+++ plantcv/extras/thermal/flir_convert.py
@@ -9,8 +9,7 @@ from flirextractor import FlirExtractor
 
 
 def flir_convert(pseudo_dir, csv_dir, thermal_index=None, contains_str=None):
-    """
-    Function that converts pseudo colored thermal images into csv files
+    """ Function that converts pseudo colored thermal images into csv files
     INPUTS:
     1) pseudo_dir: path to a directory with thermal/pseudo colored (and rgb) images
     2) csv_dir: output directory where csv files are saved

b5320dcce9abe14b527334ea53cb0ab443167f55
AFis-245 <annika.fischer@e-box3.de>
2025-06-03 11:26:32 -0500

    added helper functions to flir_convert.py

--- plantcv/extras/thermal/flir_convert.py
+++ plantcv/extras/thermal/flir_convert.py
@@ -5,12 +5,12 @@ from plantcv.plantcv import fatal_error
 import numpy as np
 import os
 import re
+from flirextractor import FlirExtractor
 
 
 def flir_convert(pseudo_dir, csv_dir, thermal_index=None, contains_str=None):
     """
     Function that converts pseudo colored thermal images into csv files
-
     INPUTS:
     1) pseudo_dir: path to a directory with thermal/pseudo colored (and rgb) images
     2) csv_dir: output directory where csv files are saved
@@ -32,43 +32,51 @@ def flir_convert(pseudo_dir, csv_dir, thermal_index=None, contains_str=None):
     if not os.path.exists(pseudo_dir):
         fatal_error(pseudo_dir + " could not be found")
 
-    # creates a list of all thermal images in img_dir
-    list_all = [f for f in os.listdir(pseudo_dir) if f.endswith('.jpg')]
-    # selects all images that end with an even number
-    # this only checks the first integer
-    temp_list = list_all
-    if thermal_index == 'even':
-        temp_list = [f for f in list_all if int(re.findall(r'\d+', f)[0]) % 2 == 0]
-    # selects all images that end with an odd number
-    # this only checks the first integer
-    elif thermal_index == 'odd':
-        temp_list = [f for f in list_all if int(re.findall(r'\d+', f)[0]) % 2 != 0]
-
-    # filter list by contain_str
-    thermal_list = temp_list
-    if contains_str is not None:
-        thermal_list = [f for f in temp_list if contains_str in f]
-
+    # filter images based on extension and optional criteria
+    thermal_list = _filter_images(pseudo_dir, thermal_index, contains_str)
     failed_files = []
     # loops through all images in thermal_list and converts them to .csv files,
     # saves outputs in csv_dir
-    from flirextractor import FlirExtractor
     # recommended to use 'with:' context manager
     with FlirExtractor() as extractor:
         for image in thermal_list:
             try:
-                new_name = image[:-4]      # removes extension .jpg
-                old_path = os.path.join(pseudo_dir, image)      # combines path to directory with filename
-                thermal_data = extractor.get_thermal(old_path)      # extracts the temperature data from the .jpg
-                new_path = os.path.join(csv_dir, new_name) + ".csv"     # combines new directory, new name and new extension
-            # combines new directory, new name and new extension if filename already exists and adds "_new" to name
-                if os.path.exists(new_path):
-                    new_path = os.path.join(csv_dir, new_name) + "_new" + ".csv"
-                np.savetxt(new_path, thermal_data, delimiter=",")       # saves temperature data in a csv file
+                _convert_image(image, pseudo_dir, csv_dir, extractor)
             except Exception as e:
-                failed_files.append(f"{old_path}: {e}")
+                failed_files.append(f"{os.path.join(pseudo_dir, image)}: {e}")
     # prints filenames and error messages of files that failed to process
     if failed_files:
         print("\n some files failed to process:")
         print("\n".join(failed_files))
     return failed_files
+
+
+def _filter_images(pseudo_dir, thermal_index, contains_str):
+    """helper function that filters images in a directory based on index and substring"""
+    # creates a list of all thermal images in img_dir
+    thermal_list = [f for f in os.listdir(pseudo_dir) if f.endswith('.jpg')]
+
+    if thermal_index in ('even', 'odd'):  # this only checks the first integer
+        # selects all images that end with an even number
+        thermal_list = [f for f in thermal_list if int(re.findall(r'\d+', f)[0]) % 2 == 0]
+        # selects all images that end with an odd number
+        if thermal_index == 'odd':
+            thermal_list = [f for f in thermal_list if int(re.findall(r'\d+', f)[0]) % 2 != 0]
+
+    # filter list by contain_str
+    if contains_str:
+        thermal_list = [f for f in thermal_list if contains_str in f]
+
+    return thermal_list
+
+
+def _convert_image(image, pseudo_dir, csv_dir, extractor):
+    """helper function to extract thermal data and save into csv"""
+    new_name = image[:-4]      # removes extension .jpg
+    old_path = os.path.join(pseudo_dir, image)      # combines path to directory with filename
+    thermal_data = extractor.get_thermal(old_path)      # extracts the temperature data from the .jpg
+    new_path = os.path.join(csv_dir, new_name) + ".csv"     # combines new directory, new name and new extension
+    # combines new directory, new name and new extension if filename already exists and adds "_new" to name
+    if os.path.exists(new_path):
+        new_path = os.path.join(csv_dir, new_name) + "_new" + ".csv"
+    np.savetxt(new_path, thermal_data, delimiter=",")       # saves temperature data in a csv file
